066b5ac96b7cbb94fff107dc467bda0e6ec418a8
kevin <kevin891104@gmail.com>
2025-03-05 00:55:56 +0800

    20250304 night

--- lecture_2.py
+++ lecture_2.py
@@ -5,16 +5,16 @@ from utility import *
 from math import *
 
 # note x does not include boundary
-N = 11
-x_min = -5
-x_max = 5
+N = 101
+x_min = -0.1
+x_max = 0.1
 dx = (x_max-x_min)/(N-1)
 x = np.zeros(N)
 for i in range(N):
     x[i] = x_min + dx*i
 
-def f(x):
-    return x**2
+# def f(x):
+#     return x**2
 
 
 
@@ -31,55 +31,57 @@ b = [[1],
      [0]]
 a = np.dot(np.linalg.inv(A),b)
 
-
-
-U_start = -asinh(0) 
-U_end = -asinh(0) 
+# boundary condition
+f= (10**5)*np.sign(x) 
+ploting(x,f,'x','f',filename='f')
+U_start = -np.arcsinh(-10**5) 
+U_end = -np.arcsinh(10**5) 
 # suppose ND = NA
 
 Up = np.array([])
-for i in x:
-    Up = np.append(Up,-np.arcsinh(i))
+for i in range(len(x)):
+    Up = np.append(Up,-np.arcsinh(f[i]))
 
 ploting(x,Up,'x','Up_initital guess',filename='Up')
-print(Up)
+# print(Up)
 
-iter = 1
+iter = 20
 for j in range(iter):
 
     # building dF/dU
     D = np.zeros((N,N))
     for r in range(N):
-        print(r)
+        # print(r)
         for c in range(N):
-            
-            print(c)
+            # print(c)
             if (r==c ):
-                D[r,c] = -2/dx**2 - np.sinh(Up[r])
+                D[r,c] = -2/dx**2 - np.cosh(Up[r])
             if (r==(c-1) ):
                 D[r,c] = 1/dx**2
             if (r==(c+1) and (not r==0) and (not r==N-1) ):
                 D[r,c] = 1/dx**2
+    D = (-D)
 
     # Gaussian Elimination
     for p in range(1,N):
         D[p,:] = D[p-1,:] * (-D[p,p-1]/D[p-1,p-1]) + D[p,:]
     
+    F = np.zeros(N)
+    F[0] = ( 1/dx**2)*(Up[1]-2*Up[0]+U_start)-np.sinh(Up[0])-f[0]
+    F[N-1] = ( 1/dx**2)*(U_end-2*Up[N-1]+Up[N-2])-np.sinh(Up[N-1])-f[N-1]
+    for i in range(2,N-1):
+        F[i] = 1/dx**2 * ( Up[i+1]-2*Up[i]+Up[i-1] ) - np.sinh(Up[i]) - f[i]
+
     # solving equation
     dU = np.zeros(N)
-    dU[N-1] = Up[N-1] / D[N-1,N-1]
+    dU[N-1] = F[N-1] / D[N-1,N-1]
     for i in range(1,N):
-        s = 0
-        for k in range(i):
-            s += D[N-i-1,N-i-k]*dU[N-i-k]
-        print(s)
-        dU[N-i-1] = 1/D[N-2,N-2] * (Up[N-2] -s )
+        dU[N-i-1] = 1/D[N-i-1,N-i-1] * (F[N-i-1] - D[N-i-1,N-i]*dU[N-i])
     
     # update Up
-    Up += dU
-    print(Up)
+    Up = Up+dU
     np.savetxt("D.txt", D, fmt="%.8f", delimiter="\t")
 
-
+ploting(x,Up,'x',"Up","Up_renew.png")
 # np.savetxt("D.txt", D, fmt="%.8f", delimiter="\t")
 
